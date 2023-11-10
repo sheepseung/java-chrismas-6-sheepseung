a1f36ff11ee13b51ddfaaad24286ce6b97efe4b3
sheepseung <qkffldlfdjsk12@naver.com>
2023-11-10 23:29:51 +0900

    refator: 파싱 및 유효성 검증 관심사 전임

--- src/main/java/christmas/domain/Order.java
+++ src/main/java/christmas/domain/Order.java
@@ -2,15 +2,20 @@ package christmas.domain;
 
 import christmas.enums.ErrorMessage;
 import christmas.enums.Menu;
+import christmas.parser.Parser;
 
 public class Order {
     private Menu menu;
     private int count;
 
-    public Order(Menu menu, int count) {
-        validateCount(count);
-        this.menu = menu;
-        this.count = count;
+    public Order(String menu, String count) {
+        int countValue = Parser.stringToIntPaser(count);
+        Menu menuValue = Menu.findMenu(menu);
+
+        validateCount(countValue);
+
+        this.menu = menuValue;
+        this.count = countValue;
     }
 
     private void validateCount(int count){
