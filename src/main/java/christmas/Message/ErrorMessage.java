9a04ee8e9936837988b16daf01dfe40f2c7b3281
sheepseung <qkffldlfdjsk12@naver.com>
2023-11-10 18:07:12 +0900

    feat: 입력값을 정수겂으로 변환 및 유효성 검증 하는 Parser클래스 구현

--- src/main/java/christmas/Message/ErrorMessage.java
+++ src/main/java/christmas/Message/ErrorMessage.java
@@ -1,2 +1,16 @@
-package christmas.Message;public enum ErrorMessage {
+package christmas.Message;
+
+public enum ErrorMessage {
+    INPUT_NOT_INTEGER_ERROR_MESSAGE("[ERROR] : 정확한 값을 입력해 주세요."),
+    INPUT_NULL_ERROR_MESSAGE("[ERROR] : 정확한 값을 입력해 주세요.")
+    ;
+
+    private final String message;
+    ErrorMessage(String message){
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
