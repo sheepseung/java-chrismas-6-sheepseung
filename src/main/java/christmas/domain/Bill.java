6dabcad90329af38ed19f15e4467409dfbe31ac3
sheepseung <qkffldlfdjsk12@naver.com>
2023-11-11 16:38:30 +0900

    feat: 최소 주문 금액 검증 구현

--- src/main/java/christmas/domain/Bill.java
+++ src/main/java/christmas/domain/Bill.java
@@ -5,12 +5,15 @@ import christmas.dto.OrderedMenu;
 import java.math.BigDecimal;
 
 public class Bill {
+    private static final int MINIMUM_AMOUNT = 10000;
+
     private final Order order;
     private BigDecimal totalPrice;
 
     public Bill(Order order){
         this.order = order;
         calculateTotalPrice();
+        validMinimumAmount();
     }
 
     private void calculateTotalPrice(){
@@ -18,4 +21,11 @@ public class Bill {
             totalPrice.add(orderedMenu.getMenu().getPrice());
         }
     }
+
+    private void validMinimumAmount(){
+        if(totalPrice.compareTo(new BigDecimal(MINIMUM_AMOUNT)) == -1){
+            throw new IllegalArgumentException("[ERROR] : 최소 주문 금액을 확인해주세요.");
+        }
+    }
+
 }
