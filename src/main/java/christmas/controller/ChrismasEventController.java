f246ffd707d17653d3b0b83900df123249d2a747
sheepseung <qkffldlfdjsk12@naver.com>
2023-11-13 11:08:02 +0900

    feat: Order, Bill 객체 의존성 주입

--- src/main/java/christmas/controller/ChrismasEventController.java
+++ src/main/java/christmas/controller/ChrismasEventController.java
@@ -1,13 +1,42 @@
 package christmas.controller;
 
+import christmas.domain.Bill;
 import christmas.domain.DecemberCalendar;
+import christmas.domain.Order;
 
 public class ChrismasEventController {
     private final DecemberCalendar decemberCalendar;
+    private final Order order;
+    private final Bill bill;
 
-    public ChrismasEventController() {
+    public ChrismasEventController(Order order) {
         this.decemberCalendar = new DecemberCalendar();
+        this.order = order;
+        this.bill = new Bill(order);
     }
 
+    public void dDayDiscountEvent(){
+
+    }
+
+    public void weekDayDiscountEvent(){
+
+    }
+
+    public void weekEndDayDiscount(){
+
+    }
+
+    public void specialDiscountEvent(){
+
+    }
+
+    public void presentEvent(){
+
+    }
+
+    public void badgeEvent(){
+
+    }
 
 }
