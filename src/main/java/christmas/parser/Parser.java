6ad7c5ebee9185786fcd681f0ade77e805eef3dd
sheepseung <qkffldlfdjsk12@naver.com>
2023-11-14 00:12:13 +0900

    refator: BigDecimal값 포맷팅 기능 전임

--- src/main/java/christmas/parser/Parser.java
+++ src/main/java/christmas/parser/Parser.java
@@ -2,12 +2,15 @@ package christmas.parser;
 
 import christmas.enums.ErrorMessage;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.util.Arrays;
 import java.util.List;
 
 public class Parser {
     private final static String INPUT_TO_EACH_ORDER_REGEX = ",";
     private final static String EACH_ORDER_TO_PER_ORDER_REGEX = "-";
+    private static final DecimalFormat DECIMAL_FORMAT = new DecimalFormat("#,###");
 
     public static int stringToIntPaser(String input) {
         if (input.isBlank()) throw new IllegalArgumentException(ErrorMessage.INPUT_NULL_ERROR_MESSAGE.getMessage());
@@ -30,5 +33,9 @@ public class Parser {
             throw new IllegalArgumentException(ErrorMessage.ORDER_DIFFERENT_FORMAT_ERROR_MESSAGE.getMessage());
 
         return menuInfo;
+    }
+
+    public static String BigDecimalToFormmatedString(BigDecimal price){
+        return DECIMAL_FORMAT.format(price);
     }
 }
