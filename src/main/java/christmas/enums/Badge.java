ba9d3f7014e9479c1c4f50dd142d91dce8234483
sheepseung <qkffldlfdjsk12@naver.com>
2023-11-14 01:24:07 +0900

    feat: 배지 enum 클래스 생성

---
+++ src/main/java/christmas/enums/Badge.java
@@ -0,0 +1,30 @@
+package christmas.enums;
+
+import java.math.BigDecimal;
+
+public enum Badge {
+    SANTA_BADGE("산타", new BigDecimal(20000)),
+    TREE_BADGE("트리", new BigDecimal(10000)),
+    STAR_BADGE("별", new BigDecimal(5000));
+
+    private final String badge;
+    private final BigDecimal standardAmount;
+
+    Badge(String badge, BigDecimal standardAmount) {
+        this.badge = badge;
+        this.standardAmount = standardAmount;
+    }
+
+    public static String getBadge(BigDecimal totalBenefit) {
+        if (totalBenefit.compareTo(SANTA_BADGE.standardAmount) == 1) {
+            return SANTA_BADGE.badge;
+        }
+        if (totalBenefit.compareTo(TREE_BADGE.standardAmount) == 1) {
+            return TREE_BADGE.badge;
+        }
+        if(totalBenefit.compareTo(STAR_BADGE.standardAmount) == 1){
+            return STAR_BADGE.badge;
+        }
+        return null;
+    }
+}
