776d9d5dd0f73c6558e6cf2b40c4a9b89f0d9673
sheepseung <qkffldlfdjsk12@naver.com>
2023-11-15 01:08:47 +0900

    refator: EventView 이벤트 관련 View 로직 분리

--- src/main/java/christmas/view/EventView.java
+++ src/main/java/christmas/view/EventView.java
@@ -1,7 +1,7 @@
 package christmas.view;
 
 import christmas.domain.Bill;
-import christmas.enums.OutputMessage;
+import christmas.enums.EventMessage;
 import christmas.parser.Parser;
 
 import java.math.BigDecimal;
@@ -9,45 +9,45 @@ import java.math.BigDecimal;
 public class EventView {
 
     public static void printPriceBeforeDiscount(Bill bill) {
-        System.out.printf(OutputMessage.BEFORE_DISCOUNT_PRICE_OUTPUT_MESSAGE.getMessage(),
+        System.out.printf(EventMessage.BEFORE_DISCOUNT_PRICE_OUTPUT_MESSAGE.getMessage(),
                 Parser.BigDecimalToFormmatedString(bill.getTotalPrice()));
     }
 
     public static void printPresentDetails(boolean canPresent){
-        System.out.println(OutputMessage.PRESENT_EVENT_OUTPUT_MESSAGE.getMessage());
+        System.out.println(EventMessage.PRESENT_EVENT_OUTPUT_MESSAGE.getMessage());
         if(canPresent) {
-            System.out.println(OutputMessage.PRESENT_ITEM.getMessage());
+            System.out.println(EventMessage.PRESENT_ITEM.getMessage());
             return;
         }
-        System.out.println(OutputMessage.NO_APPLIED_VALUE.getMessage());
+        System.out.println(EventMessage.NO_APPLIED_VALUE.getMessage());
     }
 
     public static void printEventDiscountDetails(String discountDetails){
-        System.out.println(OutputMessage.EVENT_DISCOUNT_CONTENTS_MESSAGE.getMessage());
+        System.out.println(EventMessage.EVENT_DISCOUNT_CONTENTS_MESSAGE.getMessage());
         if(!discountDetails.isBlank()){
             System.out.println(discountDetails);
             return;
         }
-        System.out.println(OutputMessage.NO_APPLIED_VALUE.getMessage());
+        System.out.println(EventMessage.NO_APPLIED_VALUE.getMessage());
     }
 
     public static void printTotalBenefitAmount(BigDecimal totalBenefit){
-        System.out.println(OutputMessage.TOTAL_BENEFIT_OUTPUT_MESSAGE.getMessage());
+        System.out.println(EventMessage.TOTAL_BENEFIT_OUTPUT_MESSAGE.getMessage());
         if(totalBenefit.compareTo(new BigDecimal(0)) == 1) {
             System.out.println("-" + Parser.BigDecimalToFormmatedString(totalBenefit) + "\n");
             return;
         }
-        System.out.println(OutputMessage.NO_APPLIED_VALUE.getMessage());
+        System.out.println(EventMessage.NO_APPLIED_VALUE.getMessage());
     }
 
     public static void printPriceAfterDiscount(Bill bill){
-        System.out.printf(OutputMessage.AFTER_DISCOUNT_PRICE_OUTPUT_MESSAGE.getMessage(),
+        System.out.printf(EventMessage.AFTER_DISCOUNT_PRICE_OUTPUT_MESSAGE.getMessage(),
                 Parser.BigDecimalToFormmatedString(bill.getTotalPrice()));
     }
 
     public static void printBadge(String badge){
-        System.out.printf(OutputMessage.BADGE_EVENT_MESSAGE.getMessage());
-        if(badge == null) System.out.println(OutputMessage.NO_APPLIED_VALUE);
+        System.out.printf(EventMessage.BADGE_EVENT_MESSAGE.getMessage());
+        if(badge == null) System.out.println(EventMessage.NO_APPLIED_VALUE);
         System.out.println(badge + '\n');
     }
 }
