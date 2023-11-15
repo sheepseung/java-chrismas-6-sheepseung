d43537b179ef17dcd777d0120f49845d5fe799a9
sheepseung <qkffldlfdjsk12@naver.com>
2023-11-15 21:37:29 +0900

    style: Menu 테스트 코드 Display 적용

--- src/test/java/christmas/domain/MenuTest.java
+++ src/test/java/christmas/domain/MenuTest.java
@@ -1,5 +1,6 @@
 package christmas.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,6 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class MenuTest {
 
     @Test
+    @DisplayName("메뉴_찾기_함수_테스트")
     void findMenu_ExistingMenuName_ShouldReturnMenu() {
         String existingMenuName = "티본스테이크";
         Menu foundMenu = Menu.findMenu(existingMenuName);
@@ -15,6 +17,7 @@ class MenuTest {
     }
 
     @Test
+    @DisplayName("메뉴_찾기_함수_예외_테스트")
     void findMenu_NonExistingMenuName_ShouldThrowException() {
         String nonExistingMenuName = "피자";
         assertThrows(IllegalArgumentException.class, () -> Menu.findMenu(nonExistingMenuName));
