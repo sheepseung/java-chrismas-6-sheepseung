68ffc07d78a666c0029fffb7b97ae6b72fc8a4b1
sheepseung <qkffldlfdjsk12@naver.com>
2023-11-15 20:46:15 +0900

    test: Order 도메인 테스트 코드 작성

--- src/test/java/christmas/domain/OrderTest.java
+++ src/test/java/christmas/domain/OrderTest.java
@@ -1,44 +1,75 @@
-//package christmas.domain;
-//
-//import christmas.dto.OrderedMenu;
-//import christmas.enums.ErrorMessage;
-//import org.junit.jupiter.api.Test;
-//
-//import java.util.List;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//public class OrderTest {
-//    Order testOrder = new Order();
-//
-//    @Test
-//    public void testTakeOrderAndToString() {
-//        testOrder.takeOrder("양송이수프-1,티본스테이크-2,초코케이크-3");
-//
-//        List<OrderedMenu> orderDetails = testOrder.getOrderDetails();
-//        assertEquals(3, orderDetails.size());
-//
-//        String expectedOutput = "양송이수프 1개\n티본스테이크 2개\n초코케이크 3개\n";
-//        assertEquals(expectedOutput, testOrder.toString());
-//    }
-//
-//    @Test
-//    public void testValidDuplicateMenu() {
-//        String input = "양송이수프-1,바비큐립-2,양송이수프-3";
-//
-//        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, ()
-//                -> testOrder.takeOrder("양송이수프-1,바비큐립-2,양송이수프-3"));
-//
-//        assertEquals(ErrorMessage.ORDER_ERROR_MESSAGE.getMessage(), exception.getMessage());
-//    }
-//
-//    @Test
-//    public void testValidOnlyBeverage() {
-//        String input = "레드와인-1,샴페인-2";
-//
-//        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, ()
-//                -> testOrder.takeOrder("레드와인-1,샴페인-2"));
-//
-//        assertEquals(ErrorMessage.ORDER_ERROR_MESSAGE.getMessage(), exception.getMessage());
-//    }
-//}
+package christmas.domain;
+
+import christmas.utils.ErrorMessage;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderTest {
+    String validInput = "티본스테이크-2,양송이수프-2";
+    @Test
+    void takeOrder_ValidInput_ShouldNotThrowException() {
+        Order order = new Order();
+        assertDoesNotThrow(() -> order.takeOrder(validInput));
+    }
+
+    @Test
+    void takeOrder_DuplicateMenu_ShouldThrowException() {
+        Order order = new Order();
+        String duplicateMenuInput = "티본스테이크-2,티본스테이크-2";
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> order.takeOrder(duplicateMenuInput));
+        assertEquals(ErrorMessage.INPUT_ORDER_ERROR_MESSAGE.getMessage(), exception.getMessage());
+    }
+
+    @Test
+    void takeOrder_InvalidOrderFormat_ShouldThrowException() {
+        Order order = new Order();
+        String invalidFormatInput = "티본스테이크+2";
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> order.takeOrder(invalidFormatInput));
+        assertEquals(ErrorMessage.INPUT_ORDER_ERROR_MESSAGE.getMessage(), exception.getMessage());
+    }
+
+    @Test
+    void takeOrder_ExceedMaximumQuantity_ShouldThrowException() {
+        Order order = new Order();
+        String exceedQuantityInput = "티본스테이크-20,양송이수프-10";
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> order.takeOrder(exceedQuantityInput));
+        assertEquals(ErrorMessage.INPUT_ORDER_ERROR_MESSAGE.getMessage(), exception.getMessage());
+    }
+
+    @Test
+    void takeOrder_OnlyBeverage_ValidInput_ShouldNotThrowException() {
+        Order order = new Order();
+        String onlyBeverageInput = "제로콜라-10";
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> order.takeOrder(onlyBeverageInput));
+        assertEquals(ErrorMessage.INPUT_ORDER_ERROR_MESSAGE.getMessage(), exception.getMessage());
+    }
+
+    @Test
+    void toString_OrderDetailsEmpty_ShouldReturnEmptyString() {
+        Order order = new Order();
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> order.takeOrder(""));
+        assertEquals(ErrorMessage.INPUT_ORDER_ERROR_MESSAGE.getMessage(), exception.getMessage());
+        assertEquals("", order.toString());
+    }
+
+    @Test
+    void getOrderDetails_OrderDetailsNotEmpty_ShouldReturnOrderDetails() {
+        Order order = new Order();
+        order.takeOrder(validInput);
+
+        Map<Menu, Integer> expectedOrderDetails = new HashMap<>();
+        expectedOrderDetails.put(Menu.findMenu("티본스테이크"), 2);
+        expectedOrderDetails.put(Menu.findMenu("양송이수프"), 2);
+
+        assertEquals(expectedOrderDetails, order.getOrderDetails());
+    }
+}
